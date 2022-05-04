3bb78a3ba470a8ceeac2bb3e7b81b8a5fdc69043
rohith <rohithvujjini0507@gmail.com>
2022-05-05 00:21:57 +0530

    css at tools

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UiModule } from '@bluebits/ui';
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  { path: 'products', component: ProductListComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+  ],
+
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
