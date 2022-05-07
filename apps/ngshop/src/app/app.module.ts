4050271bd356557a129f63e7931e7664b0fcb28c
rohith <rohithvujjini0507@gmail.com>
2022-05-07 12:51:20 +0530

    shared libs

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
@@ -8,6 +11,8 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { UiModule } from '@bluebits/ui';
+import {AccordionModule} from 'primeng/accordion';
+
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   { path: 'products', component: ProductListComponent }
@@ -23,7 +28,7 @@ const routes: Routes = [
     HomePageComponent,
   ],
 
-  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes), UiModule, AccordionModule],
   providers: [],
   bootstrap: [AppComponent],
 })
